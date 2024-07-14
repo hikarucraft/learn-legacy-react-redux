35488a437d1166c2342b1e4ee9c14eaf7e185517
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-07-14 23:46:55 +0900

    reduxなしの記事データ取得

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,21 @@
-import { connect } from 'react-redux'
-import type { AppComponentProps } from './types/types'
-import { increaseCount, decreaseCount } from './actions'
-import { useSelector, useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
 const App = () => {
-  const countDayo = useSelector((state) => state.countReducer.count)
-  const postsDayo = useSelector((state) => state.postsReducer.posts)
-  const dispatch = useDispatch()
-  const increase = (amount) => {
-    dispatch(increaseCount(amount))
-  }
-  const decrease = (amount) => {
-    dispatch(decreaseCount(amount))
-  }
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const data = await res.json()
+      setPosts(data)
+    }
+    getPosts()
+  }, [])
+
   return (
     <div>
       <h1>Redux Learn</h1>
-      <p>Count:{countDayo}</p>
-      <button onClick={(e) => increase(1)}>+1</button>
-      <button onClick={(e) => increase(2)}>+2</button>
-      <button onClick={(e) => decrease(1)}>-1</button>
-      <button onClick={(e) => decrease(2)}>-2</button>
       <ul>
-        {postsDayo.map((post) => (
+        {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
@@ -29,17 +23,4 @@ const App = () => {
   )
 }
 
-const mapStateToProps = (state: any) => {
-  return {
-    _countDayo: state.countReducer.count,
-    _postsDayo: state.postsReducer.posts,
-  }
-}
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    _increase: (amount) => dispatch(increaseCount(amount)),
-    _decrease: (amount) => dispatch(decreaseCount(amount)),
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
