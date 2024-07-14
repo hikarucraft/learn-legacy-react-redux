3730f477184a16d40d4d18fbbe667f928e0ffd3b
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-07-15 00:29:20 +0900

    fetchAllボタンで実装してみた

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,25 @@ import { useDispatch, useSelector } from 'react-redux'
 const App = () => {
   const dispatch = useDispatch()
   const posts = useSelector((state) => state.posts)
-
-  useEffect(() => {
-    const getPosts = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-      const data = await res.json()
-      dispatch({
-        type: 'GET_POSTS_DATA',
-        payload: data,
-      })
-    }
-    getPosts()
-  }, [dispatch])
+  const getPosts = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await res.json()
+    dispatch({
+      type: 'GET_POSTS_DATA',
+      payload: data,
+    })
+  }
 
   return (
     <div>
       <h1>Redux Learn</h1>
+      <button
+        onClick={() => {
+          getPosts()
+        }}
+      >
+        fetchAll
+      </button>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
